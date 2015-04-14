78011ce35280238c09f21b1b4f08ff40633d8276
Diego Esteves <diegoesteves@gmail.com>
2015-04-14 23:54:18 +0200

    site project

---
+++ site/mex-core/src/main/java/com/dne5/mex/OntologyHelper.java
@@ -0,0 +1,13 @@
+package com.dne5.mex;
+
+/**
+ * Created by root on 14.04.15.
+ */
+public class OntologyHelper {
+
+    public main[] (){
+
+        OntModel m = ModelFactory.createOntologyModel();
+
+    }
+}
