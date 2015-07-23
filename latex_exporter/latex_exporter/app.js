8dd5cc6b62da770fb72f8d79409226468f225266
Cirola2000 <cirobaroneto@gmail.com>
2015-07-23 17:58:34 +0200

    creating LaTeX table

--- latex_exporter/latex_exporter/app.js
+++ latex_exporter/latex_exporter/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var N3 = require('n3');
+
+var parser = N3.Parser();
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var makeLatexTable = require('./routes/makeLatexTable');
 var file = require('./routes/file');
 
 var app = express();
@@ -14,8 +18,25 @@ var app = express();
 var multer  = require('multer')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+ app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+
+
+app.use(multer({ dest: './uploads/',
+  onFileUploadComplete: function (file, req, res) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path);
+  loadTriples(file.path, function(data){
+    console.log(JSON.stringify(data));
+    res.send(JSON.stringify(data));
+    store = {};
+    store = N3.Store();
+  }
+  );
+  
+
+}}))
 
 
 // uncomment after placing your favicon in /public
@@ -26,45 +47,271 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer({ dest: './uploads/',
-onFileUploadComplete: function (file, req, res) {
-  console.log(file.fieldname + ' uploaded to  ' + file.path);
-}}))
+// app.use(multer({ dest: './uploads/',
+// onFileUploadComplete: function (file, req, res) {
+//   console.log(file.fieldname + ' uploaded to  ' + file.path);
+// }}))
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/makeLatexTable', makeLatexTable);
 app.use('/file', file);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var store = N3.Store();
+
+
+var mexCoreURI = "http://mex.aksw.org/mex-core#";
+var mexPerf = "http://mex.aksw.org/mex-perf#";
+var provURI  = "http://www.w3.org/ns/prov#";
+var rdfsURI = "http://www.w3.org/2000/01/rdf-schema#"; 
+
+var maxPerfMeasures = [mexPerf+"ClassificationMeasure", 
+  mexPerf+"PerformanceMeasure", 
+  mexPerf+"RegressionMeasure", 
+  mexPerf+"ClusteringMeasure", 
+  mexPerf+"StatisticalMeasure" ]; 
+  
+var measuresProp = [
+  mexPerf+"pearsonCorrelation",
+  mexPerf+  "chiSquare",
+  mexPerf+"error",
+  mexPerf+"kolmogorovSmirnov",
+  mexPerf+"mean",
+  mexPerf+"nemenyi",
+  mexPerf+"standardDeviation",
+  mexPerf+"wilCoxon",
+  mexPerf+"variance",
+  mexPerf+"friedman",
+  mexPerf+"kappaStatistics",
+  mexPerf+"median",
+  mexPerf+"mode",
+  mexPerf+"L2norm",
+  mexPerf+"L1norm",
+  mexPerf+"Linfnorm",
+// ];
+
+// var regressionMeasure = [
+  mexPerf+"meanAbsoluteDeviation",
+  mexPerf+"meanSquareError",
+  mexPerf+"residual",
+  mexPerf+"totalError",
+  mexPerf+"medianAbsoluteDeviation",
+  mexPerf+"relativeAbsoluteError",
+  mexPerf+"rootRelativeSquaredError",
+  mexPerf+"rootMeanSquaredError",
+  mexPerf+"correlationCoefficient",
+// ];
+
+// var clusteringMeasure = [
+  mexPerf+"chebyschevDistance",
+  mexPerf+"hammingDistance",
+  mexPerf+"euclideanDistance",
+  mexPerf+"manhattanDistance",
+  mexPerf+"genSimilarityCoerfficient",
+// ];
+
+// var classificationMeasure = [
+  mexPerf+"falseNegative",
+  mexPerf+"falsePositive",
+  mexPerf+"truePositive",
+  mexPerf+"trueNegative",
+  mexPerf+  "precision",
+  mexPerf+"fmeasure",
+  mexPerf+"recall",
+  mexPerf+"roc",
+  mexPerf+"sensivity",
+  mexPerf+"specificity",
+  mexPerf+"falseNegativeRate",
+  mexPerf+"falsePositiveRate",
+  mexPerf+"trueNegativeRate",
+  mexPerf+"truePositiveRate",
+  mexPerf+"accuracy",
+];
+  
+  
+
+var experimentConfigurationURI = "ExperimentConfiguration";
+var algorithmURI = mexCoreURI+"Algorithm";
+var wasInformedByURI = provURI + "wasInformedBy";
+var usedURI = provURI + "used";
+
+var aURI = rdfsURI+"type";
+
+
+
+
+
+
+var RDFFilePath;
+var tableTitle;
+
+
+
+function loadRDF(){
+  
+  var algorithms;
+var executions = []; 
+var measures = [];
+var wasInformedBy = [];
+
+var line = {};
+  
+  // get all measures
+   measures = (getSubjects(findTriplesBasedOnObjects(maxPerfMeasures)));
+   
+   for(var m in measures){
+     
+     var measureObj = findTriplesBasedOnSubjectAndPredicates(measures[m], measuresProp);
+     // var measureObjValues = [];
+     var measureObjValues;
+     
+     // store measure values
+     for(var mv in measureObj){
+       var measureObjValuesTmp = {};
+       measureObjValuesTmp.prop = measureObj[mv].predicate;
+       measureObjValuesTmp.val = measureObj[mv].object;
+       // measureObjValues.push(measureObjValuesTmp);
+       measureObjValues =measureObjValuesTmp;
+     }
+     
+     executions = getExecutionOfMeasure(measures[m]); 
+     
+     for (var e in executions){ 
+       
+       algorithms = getAlgorithmsOfExecution(executions[e]);
+       
+       for (var a in algorithms){
+         if(typeof line[algorithms[a]] != 'undefined'){
+           line[algorithms[a]].measures.push (measureObjValues);
+         }
+         else{
+           line[algorithms[a]] = {}; 
+           line[algorithms[a]].name = algorithms[a];
+           line[algorithms[a]].measures = [];
+           
+         }
+       } 
+     }
+   }
+   return line;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+
+function getSubjects(triples){
+  var subjects = [];
+  for (var i in triples){
+    subjects.push(triples[i].subject);
+  }
+  return subjects;
+}
+
+function getObjects(triples){
+  var objects = [];
+  for (var i in triples){
+    objects.push(triples[i].object);
+  }
+  return objects;
+}
+
+function findTriplesBasedOnObjects(objects){
+  var triples = [];
+  var triplesTMP;
+  for (var i in objects){
+    triplesTMP = store.find(null, null , objects[i]);
+    for(var v in triplesTMP){
+      triples.push(triplesTMP[v]);
+    }
+  }
+  return triples; 
+}
+
+function findTriplesBasedOnSubjectAndPredicates(subject, predicates){
+  var triples = [];
+  var triplesTMP;
+  for (var i in predicates){
+    triplesTMP = store.find(subject, predicates[i] , null);
+    for(var v in triplesTMP){
+      triples.push(triplesTMP[v]);
+    }
+  }
+  return triples; 
+}
+
+function getExecutionOfMeasure(measure){
+  return getObjects(store.find(measure, wasInformedByURI , null));
+}
+
+function getAlgorithmsOfExecution(execution){
+  return getObjects(store.find(execution, usedURI , null));
+}
+
+function loadTriples(fileName, callback){
+   fs.readFile(fileName, "utf8", function (err, data) {
+      if (err) throw err;
+      parser.parse(data,
+          function (error, triple, prefixes) {
+            if (triple){
+              store.addTriple(triple.subject, triple.predicate, triple.object);
+            }
+            else{
+              return callback(loadRDF());
+            }
+          });
+             
+       
+    }); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = app;
--- latex_exporter/latex_exporter/public/javascripts/latex_exporter.js
+++ latex_exporter/latex_exporter/public/javascripts/latex_exporter.js
@@ -1,13 +1,113 @@
-console.log("POR");
-$('#uploadForm').submit(function() {
+
+var main = angular.module('main', ["ngRoute"]);
+
+main.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+      when('/partial', {
+      templateUrl: '/partial/part1',
+      controller: 'partCtrl'
+    }).
+    // when('/', {
+    //  templateUrl: '/index.html',
+    //  controller: 'rootCtrl'
+    // }).
+      otherwise({
+      redirectTo: '/'
+    });
+  }]);
+
+
+
+
+// main.controller('partCtrl', ['$scope', function ($scope) {
+//   $scope.data2 = 'Hola!';
+// }]);
+
+main.controller('rootCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.lines = [];
+  $scope.latexTableView;
+  $scope.columns = [];
+  $scope.table = [[]];
+  $scope.title = [];
+
+
+  console.log($scope.table);
+
+  var indexLines = 0;
+  var indexColumns = 0;
+
+  $scope.newLine = function () {
+    if ($scope.lines.length > 0) {
+      indexLines++;
+      $scope.lines.push({ index: indexLines, val: $scope.algorithms });
+    }
+  };
+
+  $scope.newColumn = function () {
+    if ($scope.columns.length > 0) {
+      indexColumns++;
+      $scope.columns.push(indexColumns);
+    }
+  };
+
+  $scope.updateTitle = function (line, data) {
+    $scope.title[line] = data;
+  };
+
+  $scope.updateTable = function (line, column, data) {
+    for (var i = 0; i <= indexLines; i++) {
+      for (var j = 0; j <= indexColumns + 1; j++) {
+        if (typeof $scope.table[i] == 'undefined') {
+          $scope.table[i] = [];
+        }
+        if (typeof $scope.table[i][j] == 'undefined') {
+          $scope.table[i][j] = '-';
+        }
+        if (i == line && j == column) {
+          $scope.table[line][column] = data;
+        }
+      }
+    }
+    $scope.showTable();
+  };
+
+
+  $scope.showTable = function () {  
+    $http({
+      method: 'post',
+      url: 'makeLatexTable',
+      data: { table: $scope.table},
+    }).success(function (data, status, headers, config) {
+      $scope.latexTableView = data;
+      console.log(data);
+      // $scope.$apply();
+    }).error(function (data, status, headers, config) {
+    });
+  };
+
+
+
+
+  $('#uploadForm').submit(function () {
     $(this).ajaxSubmit({
-        error: function(xhr) {
-           status('Error: ' + xhr.status);
-       },
-       success: function(response) {
-         console.log(response);
-     }
- });
+      error: function (xhr) {
+        status('Error: ' + xhr.status);
+      },
+      success: function (response) {
+        $scope.algorithms = $.parseJSON(response);
+        indexLines = 0;
+        indexColumns = 0;
+        $scope.lines.push({ index: indexLines, val: $scope.algorithms });
+        $scope.columns.push(indexColumns);
+        $scope.$apply();
+      }
+    });
     //Very important line, it disable the page refresh.
     return false;
-}); 
+  });
+
+
+
+}]);
+
