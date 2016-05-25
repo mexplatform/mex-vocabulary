833f3420db2cd4bedfc393f0775466ccb550b516
Diego Esteves <esteves@informatik.uni-leipzig.de>
2016-05-25 23:18:33 +0200

    LICENSE

--- core/src/main/java/org/aksw/mex/util/MEXEnum.java
+++ core/src/main/java/org/aksw/mex/util/MEXEnum.java
@@ -1,4 +1,24 @@
-package org.aksw.mex.util; /**
+/**
+ * Copyright (C) 2014 - 2016, Diego Esteves
+ *
+ * This file is part of LOG4MEX.
+ *
+ * LOG4MEX is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOG4MEX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.aksw.mex.util;
+/**
  * Created by esteves on 26.06.15.
  */
 
